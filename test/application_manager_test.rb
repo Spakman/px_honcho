f1284b2c74e370126d63ca1670fcec3d420750ec
Mark Somerville <mark@scottishclimbs.com>
2009-10-22 15:50:18 +0100

    Added a test for handling a <closing 0> response.

--- test/application_manager_test.rb
+++ test/application_manager_test.rb
@@ -18,40 +18,40 @@ class ApplicationManagerTest < Test::Unit::TestCase
   FakeEventListener = Struct.new :queue
 
   def teardown
-    FileUtils.rm_f "/tmp/simple.socket"
-    FileUtils.rm_f "/tmp/just_as_simple.socket"
+    FileUtils.rm_f "/tmp/no_respond.socket"
+    FileUtils.rm_f "/tmp/respond_keep_focus.socket"
     sleep 1
     @manager.shutdown
   end
 
   def test_load_first_application
     @manager = Honcho::ApplicationManager.new FakeRenderArbiter.new(Queue.new), nil
-    @manager.load_application "simple"
+    @manager.load_application "no_respond"
     assert_equal 1, @manager.applications.size
-    assert_equal "simple", @manager.applications.active[:name]
+    assert_equal "no_respond", @manager.applications.active[:name]
     assert !@manager.applications.active[:socket].closed?
   end
 
   def test_switch_application_on_application_load
     @manager = Honcho::ApplicationManager.new FakeRenderArbiter.new(Queue.new), nil
-    @manager.load_application "simple"
-    @manager.load_application "just_as_simple"
+    @manager.load_application "no_respond"
+    @manager.load_application "respond_keep_focus"
     assert_equal 2, @manager.applications.size
-    assert_equal "just_as_simple", @manager.applications.active[:name]
-    assert !@manager.applications.running?("simple")[:socket].closed?
+    assert_equal "respond_keep_focus", @manager.applications.active[:name]
+    assert !@manager.applications.running?("no_respond")[:socket].closed?
     assert !@manager.applications.active[:socket].closed?
-    assert @manager.has_focus?("just_as_simple")
-    assert !@manager.has_focus?("simple")
+    assert @manager.has_focus?("respond_keep_focus")
+    assert !@manager.has_focus?("no_respond")
   end
 
   def test_load_already_loaded_application
     @manager = Honcho::ApplicationManager.new FakeRenderArbiter.new(Queue.new), nil
-    @manager.load_application "simple"
-    @manager.load_application "just_as_simple"
-    @manager.load_application "simple"
+    @manager.load_application "no_respond"
+    @manager.load_application "respond_keep_focus"
+    @manager.load_application "no_respond"
     assert_equal 2, @manager.applications.size
-    assert_equal "simple", @manager.applications.active[:name]
-    assert @manager.has_focus?("simple")
+    assert_equal "no_respond", @manager.applications.active[:name]
+    assert @manager.has_focus?("no_respond")
   end
 
   def test_event_loop
@@ -59,16 +59,27 @@ class ApplicationManagerTest < Test::Unit::TestCase
     queue << Honcho::InputEvent.new(:top_left)
     queue << Honcho::InputEvent.new(:jog_wheel_left)
     @manager = Honcho::ApplicationManager.new FakeRenderArbiter.new(Queue.new), FakeEventListener.new(queue)
-    @manager.load_application "simple"
+    @manager.load_application "no_respond"
     Thread.new do
       @manager.event_loop
     end
     sleep 0.2
     # Should still have one event on the queue, since the event loop should be waiting for a response
     assert_equal 1, queue.size
-    # the simple program will send a response on SIGUSR1
+    # the no_respond program will send a response on SIGUSR1
     Process.kill "USR1", @manager.applications.active[:pid]
     sleep 0.2
     assert_empty queue
+  end
+
+  def test_act_on_closing_response
+    queue = Queue.new
+    @manager = Honcho::ApplicationManager.new FakeRenderArbiter.new(Queue.new), FakeEventListener.new(queue)
+    @manager.load_application "respond_keep_focus"
+    @manager.load_application "no_respond"
+    @manager.act_on_response Honcho::Message.new(:closing)
+    assert_equal 1, @manager.applications.size
+    assert_equal "respond_keep_focus", @manager.applications.active[:name]
+    assert @manager.has_focus?("respond_keep_focus")
   end
 end

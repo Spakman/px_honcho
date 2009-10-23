973df5bfd05e14282865fe87b2c1e5b02d049f44
Mark Somerville <mark@scottishclimbs.com>
2009-10-23 12:27:17 +0100

    Added some parameters to the <passfocus> test.

--- test/message_test.rb
+++ test/message_test.rb
@@ -4,7 +4,7 @@ require_relative "../lib/message"
 class MessageTest < Test::Unit::TestCase
   def test_to_s
     assert_equal "<keepfocus 0>\n", Honcho::Message.new(:keepfocus).to_s
-    assert_equal "<passfocus 19>\napplication: my_app", Honcho::Message.new(:passfocus, { application: "my_app" }).to_s
+    assert_equal "<passfocus 50>\napplication: mozart\nmethod: play_ids\nparams: 1,2,3", Honcho::Message.new(:passfocus, { application: "mozart", method: "play_ids", params: "1,2,3" }).to_s
     assert_equal "<render 18>\n<text>hello</text>", Honcho::Message.new(:render, "<text>hello</text>").to_s
   end
 end
